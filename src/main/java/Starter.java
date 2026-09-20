ab8cc7cb0d834ddbd046915120576eb90546215c
David Nguyen <daviddmnguyen@gmail.com>
2026-09-20 02:47:39 -0700

    Added More Relationships

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -10,7 +10,7 @@ public class Starter {
 	Statement statement;
 	
 	public static void main(String[] args) throws Exception{
-		String password = [password here]
+		String password = [password here];
 		Starter starter = new Starter(DriverManager.getConnection("jdbc:mysql://localhost:3306/recipegenie?serverTimezone=UTC", "root", password));
 		
 		// Options for restarting table
@@ -27,6 +27,11 @@ public class Starter {
 	}
 
 	public void createTables() throws Exception {
+		createEntitySets();
+		createRelationshipSets();
+	}
+	
+	public void createEntitySets() throws Exception{
 		this.statement = this.connection.createStatement();
 		this.statement.execute("CREATE TABLE IF NOT EXISTS INGREDIENTS (ID_INGREDIENT INT NOT NULL AUTO_INCREMENT, " 
 				+ "INGREDIENT_NAME VARCHAR(45) NOT NULL UNIQUE, PRIMARY KEY (ID_INGREDIENT))");
@@ -47,10 +52,33 @@ public class Starter {
 				+ "DISH_TYPE_NAME VARCHAR(45) NOT NULL UNIQUE, PRIMARY KEY (ID_DISH_TYPE))");
 		this.statement.execute("CREATE TABLE IF NOT EXISTS DIETARY_RESTRICTIONS (ID_DIETARY_RESTRICTION INT NOT NULL AUTO_INCREMENT, " 
 				+ "DIETARY_RESTRICTION_NAME VARCHAR(45) NOT NULL UNIQUE, SYMBOL BLOB, PRIMARY KEY (ID_DIETARY_RESTRICTION))");
-		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_INGREDIENTS (ID_RECIPE INT NOT NULL, ID_INGREDIENT INT NOT NULL, PRIMARY KEY (ID_RECIPE, ID_INGREDIENT))");
 		this.statement.close();
 	}
 	
+	public void createRelationshipSets() throws Exception{
+		this.statement = this.connection.createStatement();
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_INGREDIENTS (ID_RECIPE INT NOT NULL, ID_INGREDIENT INT NOT NULL, "
+				+ "PRIMARY KEY (ID_RECIPE, ID_INGREDIENT))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_COOKING_APPLIANCES (ID_RECIPE INT NOT NULL, ID_COOKING_APPLIANCE "
+				+ "INT NOT NULL, PRIMARY KEY (ID_RECIPE, ID_COOKING_APPLIANCE))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_REVIEWS (ID_RECIPE INT NOT NULL, ID_REVIEW INT NOT"
+				+ " NULL, PRIMARY KEY (ID_RECIPE, ID_REVIEW))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_USER_AUTHOR (ID_RECIPE INT NOT NULL, ID_USER INT NOT"
+				+ " NULL, PRIMARY KEY (ID_RECIPE, ID_USER))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS USER_FOLLOWED__USER_FOLLOWER (ID_USER_FOLLOWED INT NOT NULL, ID_USER_FOLLOWER INT NOT"
+				+ " NULL, PRIMARY KEY (ID_USER_FOLLOWED, ID_USER_FOLLOWER))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_USER_BOOKMARKER (ID_RECIPE INT NOT NULL, ID_USER INT NOT"
+				+ " NULL, PRIMARY KEY (ID_RECIPE, ID_USER))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_NATIONALITIES (ID_RECIPE INT NOT NULL, ID_NATIONALITY INT NOT"
+				+ " NULL, PRIMARY KEY (ID_RECIPE, ID_NATIONALITY))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPES_DISH_TYPE (ID_RECIPE INT NOT NULL, ID_DISH_TYPE INT NOT"
+				+ " NULL, PRIMARY KEY (ID_RECIPE, ID_DISH_TYPE))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS INGREDIENTS_DIETARY_RESTRICTIONS (ID_INGREDIENT INT NOT NULL, ID_DIETARY_RESTRICTION INT NOT"
+				+ " NULL, PRIMARY KEY (ID_INGREDIENT, ID_DIETARY_RESTRICTION))");
+		this.statement.execute("CREATE TABLE IF NOT EXISTS RECIPE_DIETARY_RESTRICTIONS (ID_RECIPE INT NOT NULL, ID_DIETARY_RESTRICTION INT NOT"
+				+ " NULL, PRIMARY KEY (ID_RECIPE, ID_DIETARY_RESTRICTION))");
+		this.statement.close();
+	}
 	public boolean actOnAllTables(String action) throws Exception{ // False if action not done. True if action done
 		System.out.println("Are you sure you want to " + action.toLowerCase() + " all tables?"); // Verification to ensure that action is intentional
 		String response = this.scanner.next();
