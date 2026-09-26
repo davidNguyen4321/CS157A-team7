f482328eda67ccab22a5e2773769624eb54b9af1
Johnny Tsai <tsaij199@gmail.com>
2026-09-26 05:11:50 -0700

    Finish refactoring entity sets

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -103,7 +103,7 @@ public class Starter {
 				    BASE_DIETARY_RESTRICTION_ID INT NULL,
 				    FOREIGN KEY (BASE_DIETARY_RESTRICTION_ID) REFERENCES DIETARY_RESTRICTIONS (DIETARY_RESTRICTION_ID) ON DELETE SET NULL
 				)
-		        """,
+		        """, // Business Logic
 		        """
 		        CREATE TABLE IF NOT EXISTS CUISINES (
 		            CUISINE_ID INT PRIMARY KEY AUTO_INCREMENT,
@@ -112,7 +112,7 @@ public class Starter {
 		            BASE_CUISINE_ID INT NULL,
 		        	FOREIGN KEY (BASE_CUISINE_ID) REFERENCES CUISINES (CUISINE_ID) ON DELETE SET NULL
 		        )
-		        """,
+		        """, // User, AI, or blank
 		        """
 		        CREATE TABLE IF NOT EXISTS DISH_TYPES (
 				    DISH_TYPE_ID INT PRIMARY KEY AUTO_INCREMENT,
@@ -121,56 +121,85 @@ public class Starter {
 				    BASE_DISH_TYPE_ID INT NULL,
 				    FOREIGN KEY (BASE_DISH_TYPE_ID) REFERENCES DISH_TYPES (DISH_TYPE_ID) ON DELETE SET NULL
 				)
-		        """,
+		        """, // User, AI
 		        """
 		        CREATE TABLE IF NOT EXISTS USERS (
 		        	USER_ID INT PRIMARY KEY AUTO_INCREMENT,
 		        	USER_NAME VARCHAR(100) NOT NULL UNIQUE,
 		        	USER_IMAGE_PATH VARCHAR(500) NULL,
 		        	USER_DESCRIPTION TEXT NULL,
-		        	USER_TIME_DISPLAY VARCHAR(25) NOT NULL,
+		        	USER_ACTIVE_TIMESTAMP TIMESTAMP NOT NULL DEFAULT (UTC_TIMESTAMP),
+		        	USER_REMOVE_TIMESTAMP TIMESTAMP NULL,
+		        	USER_STATUS ENUM('Active', 'Deleted', 'Moderated') NOT NULL DEFAULT 'Active',
+		        	TIMEZONE VARCHAR(30) NOT NULL,
+		            PASSWORD_HASH VARCHAR(254) NOT NULL,
 		        	EMAIL_ADDRESS VARCHAR(254) NULL UNIQUE,
-		        	PHONE_NUMBER VARCHAR(25) NULL UNIQUE,
+		        	PHONE_NUMBER VARCHAR(15) NULL UNIQUE,
 		        	CHECK (COALESCE (EMAIL_ADDRESS, PHONE_NUMBER) IS NOT NULL)
-		        	PASSWORD_HASH VARCHAR(254) NOT NULL,
-		        	JOIN_TIMESTAMP TIMESTAMP NOT NULL DEFAULT (UTC_TIMESTAMP)
 		        )
-		        """,
+		        """, // Soft delete only
 		        """
 		        CREATE TABLE IF NOT EXISTS RECIPES (
 		            RECIPE_ID INT PRIMARY KEY AUTO_INCREMENT,
 		            RECIPE_NAME VARCHAR(100) NOT NULL,
 		            RECIPE_IMAGE_PATH VARCHAR(500) NULL,
 		            RECIPE_DESCRIPTION TEXT NULL,
+		            RECIPE_ACTIVE_TIMESTAMP TIMESTAMP NOT NULL DEFAULT (UTC_TIMESTAMP),
+		            RECIPE_REMOVE_TIMESTAMP TIMESTAMP NULL,
+		            RECIPE_STATUS ENUM('Active', 'Deleted', 'Moderated') NOT NULL DEFAULT 'Active',
 		            INSTRUCTIONS TEXT NOT NULL,
 		            TOTAL_MINUTES INT NULL,
-		            WHEN_TO_EAT VARCHAR(25) NULL,
-		            COURSE VARCHAR(25) 
-		            
-		            
-		            
-		            COURSE ENUM('Appetizer', 'Main Course', 'Side Dish', 'Dessert') NULL,
-		            PUBLISH_DATE DATE NOT NULL DEFAULT (CURRENT_DATE),
-		            AUTHOR_ID INT NOT NULL,
-		            FOREIGN KEY (AUTHOR_ID) REFERENCES USERS (USER_ID) ON DELETE CASCADE,
+		            MINUTES_AI_GENERATED BOOLEAN NOT NULL,
+		            COURSE ENUM('Entrée', 'Appetizer / Side', 'Dessert', 'Other') NOT NULL,
+		            AUTHOR_ID INT NULL,
+		            FOREIGN KEY (AUTHOR_ID) REFERENCES USERS (USER_ID)
 		        )
-		        """,
+		        """, // Hard delete for 'Deleted' after 30 days, for 'Moderated' after violation expires
 		        """
 		        CREATE TABLE IF NOT EXISTS REVIEWS (
-		            REVIEW_ID INT NOT NULL AUTO_INCREMENT,
+		            REVIEW_ID INT PRIMARY KEY AUTO_INCREMENT,
+		            REVIEW_IMAGE_PATH VARCHAR(500) NULL,
+		            REVIEW_DESCRIPTION TEXT NOT NULL,
+		            REVIEW_ACTIVE_TIMESTAMP TIMESTAMP NOT NULL DEFAULT (UTC_TIMESTAMP),
+		            REVIEW_REMOVE_TIMESTAMP TIMESTAMP NULL,
+		            REVIEW_STATUS ENUM('Active', 'Deleted', 'Moderated') NOT NULL DEFAULT 'Active',
 		            RATING INT NOT NULL,
 		            CHECK (RATING BETWEEN 1 AND 5),
-		            CONTENT TEXT NULL,
-		            PHOTO_PATH VARCHAR(150) NULL,
-		            POST_DATE DATE NOT NULL DEFAULT (CURRENT_DATE),
-		            RECIPE_ID INT NOT NULL,
-		        	USER_ID INT NOT NULL,
-		            PRIMARY KEY (REVIEW_ID),
-		            FOREIGN KEY (RECIPE_ID) REFERENCES RECIPES (RECIPE_ID) ON DELETE CASCADE,
-		        	FOREIGN KEY (USER_ID) REFERENCES USERS (USER_ID) ON DELETE CASCADE,
-		        	UNIQUE (RECIPE_ID, USER_ID)
+		            REF_RECIPE_ID INT NOT NULL,
+		            FOREIGN KEY (REF_RECIPE_ID) REFERENCES RECIPES (RECIPE_ID) ON DELETE CASCADE,
+		        	REVIEWER_ID INT NOT NULL,
+		        	FOREIGN KEY (REVIEWER_ID) REFERENCES USERS (USER_ID),
+		        	UNIQUE (REF_RECIPE_ID, REVIEWER_ID)
 		        )
+		        """, // Hard delete for 'Deleted' after 30 days, for 'Moderated' after violation expires
 		        """
+		        CREATE TABLE IF NOT EXISTS REPORTS (
+				    REPORT_ID INT PRIMARY KEY AUTO_INCREMENT,
+				    REPORT_DESCRIPTION TEXT NULL,
+				    REPORT_ACTIVE_TIMESTAMP TIMESTAMP NOT NULL DEFAULT (UTC_TIMESTAMP),
+				    REPORT_STATUS ENUM('Active', 'Resolved', 'Dismissed') NOT NULL DEFAULT 'Active',
+				    CONTENT_TYPE ENUM('Recipe', 'Review', 'User') NOT NULL,
+				    REASON ENUM('Spam', 'Vulgar Content', 'Malicious Links', 'Copyright', 'Misinformation', 'Harassment', 'Scam', 'Other') NOT NULL,
+				    EVIDENCE JSON NOT NULL,
+				    REPORTED_ID INT NOT NULL,
+				    FOREIGN KEY (REPORTED_ID) REFERENCES USERS (USER_ID),
+				    REPORTER_ID INT NOT NULL,
+				    FOREIGN KEY (REPORTER_ID) REFERENCES USERS (USER_ID)
+				)		
+		        """, // Never deleted
+		        """
+			    CREATE TABLE IF NOT EXISTS VIOLATIONS (
+			        VIOLATION_ID INT PRIMARY KEY AUTO_INCREMENT,
+			        VIOLATION_DESCRIPTION TEXT NOT NULL,
+			        VIOLATION_ACTIVE_TIMESTAMP TIMESTAMP NOT NULL DEFAULT (UTC_TIMESTAMP),
+				    SEVERITY ENUM('Minor', 'Moderate', 'Severe') NOT NULL,
+				    IS_PERMANENT BOOLEAN NOT NULL,
+				    VIOLATOR_ID INT NOT NULL,
+				    FOREIGN KEY (VIOLATOR_ID) REFERENCES USERS (USER_ID),
+				    REPORT_ID INT NULL,
+				    FOREIGN KEY (REPORT_ID) REFERENCES REPORTS (REPORT_ID)
+				)
+		        """ // 'Minor' and 'Moderate' expire unless violator banned
 		};
 		
 		executeAll(codeblocks);
